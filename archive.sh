193e37bc87aee25cb58f2d11c317f963d378b310
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-02-07 00:10:30 +0100

    Add also offers plugin to tar
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- archive.sh
+++ archive.sh
@@ -8,6 +8,6 @@ outputtar=${TARGET_HOST/v7a/}_lightning_ndk.tar
 tar -cf ${outputtar} -C bitcoin/depends/${TARGET_HOST/v7a/}/bin bitcoind bitcoin-cli
 tar -cf ${outputtar} ${BUILDROOT}/bin/tor
 tar -rf ${outputtar} -C lightning lightningd/lightning_channeld lightningd/lightning_closingd lightningd/lightning_connectd lightningd/lightning_gossipd lightningd/lightning_hsmd lightningd/lightning_onchaind lightningd/lightning_openingd lightningd/lightningd
-tar -rf ${outputtar} -C lightning plugins/autoclean plugins/keysend plugins/bcli plugins/esplora plugins/txprepare plugins/pay plugins/spenderp plugins/fetchinvoice
+tar -rf ${outputtar} -C lightning plugins/autoclean plugins/keysend plugins/bcli plugins/esplora plugins/txprepare plugins/pay plugins/spenderp plugins/fetchinvoice plugins/offers
 tar -rf ${outputtar} -C lightning cli/lightning-cli
 xz ${outputtar}
