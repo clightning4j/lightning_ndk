aadfb0114572e9c22abcd2af2680bcdea36bd2e0
Luca Vaccaro <me@lvaccaro.com>
2021-04-04 20:23:22 +0200

    update to clightning v0.10.0

--- buildlightning.sh
+++ buildlightning.sh
@@ -7,7 +7,7 @@ REPOPATH=$PWD
 # download lightning
 git clone https://github.com/ElementsProject/lightning.git lightning
 cd lightning
-git checkout v0.9.3
+git checkout v0.10.0
 
 # set virtualenv for lightning
 python3 -m virtualenv venv
@@ -33,7 +33,7 @@ patch -p1 < ${REPOPATH}/lightning-endian.patch
 
 # add esplora plugin
 git clone https://github.com/lvaccaro/esplora_clnd_plugin.git
-cd esplora_clnd_plugin && git checkout v0.9.3 && cd ..
+cd esplora_clnd_plugin && git checkout master && cd ..
 cp esplora_clnd_plugin/esplora.c plugins/
 sed -i 's/LDLIBS = /LDLIBS = -lcurl -lssl -lcrypto /g' Makefile
 patch -p1 < esplora_clnd_plugin/Makefile.patch
